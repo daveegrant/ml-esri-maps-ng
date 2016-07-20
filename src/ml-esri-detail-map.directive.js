e1b1eaf2e1bae5a40c73ab82e1a0fdfc710b8c8b
Dave Grant <Dave.Grant@marklogic.com>
2016-07-20 15:33:30 -0400

    #7 - Updated detail map directive to accept GeoJSON formatted geometry data.

--- src/ml-esri-detail-map.directive.js
+++ src/ml-esri-detail-map.directive.js
@@ -11,6 +11,7 @@
       restrict: 'E',
       scope: {
         geometry: '=',
+        geometries: '=',
         baseMap: '=',
         zoom: '=',
         mapId: '='
@@ -33,27 +34,12 @@
     ctrl.mapZoom = $scope.zoom ? $scope.zoom : 6;
     ctrl.mapGeometry = null;
 
-    // Generate objects based on the provided geometry
-    if ($scope.geometry && angular.isArray($scope.geometry)) {
-      // Use multi-point when an array-of-arrays.
-      if (angular.isArray($scope.geometry[0])) {
-        for (i=0; i < $scope.geometry.length; i++) {
-          if (!isNaN($scope.geometry[i][0]) && $scope.geometry[i].length === 2) {
-            geometryData.push({
-              'type': 'point',
-              'latitude': $scope.geometry[i][0],
-              'longitude': $scope.geometry[i][1]
-            });
-          }
-        }
-      } else if (!isNaN($scope.geometry[0]) && $scope.geometry.length === 2) {
-        geometryData.push({
-          'type': 'point',
-          'latitude': $scope.geometry[0],
-          'longitude': $scope.geometry[1]
-        });
+    if ($scope.geometry) {
+      geometryData = processGeometry($scope.geometry);
+    }
 
-      }
+    if ($scope.geometries) {
+      geometryData = processGeoJSON($scope.geometries);
     }
 
     // Have to wait for scope value of <mapId> to be updated in view
@@ -65,18 +51,147 @@
     });
 
     /**
+    *  Processes the <geometry> scope variable which can contain point data.
+    */
+    function processGeometry(geometry) {
+      var geometryData = [];
+
+      // Generate objects based on the provided geometry
+      if (geometry && angular.isArray(geometry)) {
+        // Use multi-point when an array-of-arrays.
+        if (angular.isArray(geometry[0])) {
+          for (i=0; i < geometry.length; i++) {
+            if (!isNaN(geometry[i][0]) && geometry[i].length === 2) {
+              geometryData.push({
+                'type': 'point',
+                'latitude': geometry[i][0],
+                'longitude': geometry[i][1]
+              });
+            }
+          }
+        }
+        else if (!isNaN(geometry[0]) && geometry.length === 2) {
+          geometryData.push({
+            'type': 'point',
+            'latitude': geometry[0],
+            'longitude': geometry[1]
+          });
+
+        }
+      }
+
+      return geometryData;
+    }
+
+    /**
+    *  Processes the <geometry> scope variable which can contain point data.
+    */
+    function processGeoJSON(geoJsonData) {
+      var geometryData = [];
+      var convertedData = null;
+
+      // Generate objects based on the provided geometry
+      if (geoJsonData) {
+        if (angular.isArray(geoJsonData)) {
+          for (var i=0; i < geoJsonData.length; i++) {
+            convertedData = convertSingleGeoJSON(geoJsonData[i]);
+            if (convertedData) {
+              if (angular.isArray(convertedData)) {
+                geometryData = geometryData.concat(convertedData);
+              }
+              else {
+                geometryData.push(convertedData);
+              }
+            }
+          }
+        }
+        else {
+          convertedData = convertSingleGeoJSON(geoJsonData);
+          if (convertedData) {
+            if (angular.isArray(convertedData)) {
+              geometryData = geometryData.concat(convertedData);
+            }
+            else {
+              geometryData.push(convertedData);
+            }
+          }
+        }
+      }
+
+      return geometryData;
+    }
+
+    function convertSingleGeoJSON(geoJsonData) {
+      var geometryData = null;
+
+      if (geoJsonData && geoJsonData.type) {
+        if (geoJsonData.type === 'Point') {
+          geometryData = {
+            'type': 'point',
+            'latitude': geoJsonData.coordinates[1],
+            'longitude': geoJsonData.coordinates[0]
+          };
+        }
+        else if (geoJsonData.type === 'MultiPoint') {
+          geometryData = {
+            'type': 'multipoint',
+            'points': geoJsonData.coordinates
+          };
+        }
+        else if (geoJsonData.type === 'LineString') {
+          geometryData = {
+            'type': 'polyline',
+            'paths': [ geoJsonData.coordinates ]
+          };
+        }
+        else if (geoJsonData.type === 'MultiLineString') {
+          geometryData = {
+            'type': 'polyline',
+            'paths': geoJsonData.coordinates
+          };
+        }
+        else if (geoJsonData.type === 'Polygon') {
+          geometryData = {
+            'type': 'polygon',
+            'rings': geoJsonData.coordinates
+          };
+        }
+        else if (geoJsonData.type === 'MultiPolygon') {
+          geometryData = [];
+          for (var i=0; i < geoJsonData.coordinates.length; i++) {
+            geometryData.push(
+              {
+                'type': 'polygon',
+                'rings': geoJsonData.coordinates[i]
+              }
+            );
+          }
+        }
+        else {
+          console.log('Unknown GeoJSON geometry type: ', geoJsonData.type);
+        }
+      }
+
+      return geometryData;
+    }
+
+    /**
     *  Initializes the map with a single feature layer for locations.
     */
     function initMap(geoData) {
       require([
         'esri/map', 'esri/graphic', 'esri/symbols/SimpleFillSymbol',
-        'esri/symbols/SimpleMarkerSymbol', 'esri/layers/GraphicsLayer',
+        'esri/symbols/SimpleMarkerSymbol', 'esri/symbols/SimpleLineSymbol',
+        'esri/layers/GraphicsLayer',
         'esri/geometry/Point', 'esri/geometry/Polygon',
+        'esri/geometry/Multipoint', 'esri/geometry/Polyline',
         'esri/graphicsUtils', 'esri/Color'
       ], function(
         Map, Graphic, SimpleFillSymbol,
-        SimpleMarkerSymbol, GraphicsLayer,
+        SimpleMarkerSymbol, SimpleLineSymbol,
+        GraphicsLayer,
         Point, Polygon,
+        Multipoint, Polyline,
         graphicsUtils, Color
       )
       {
@@ -109,8 +224,15 @@
             for (var i=0; i < geoData.length; i++) {
               if (geoData[i].type === 'point') {
                 ctrl.mapGeometry.push( new Point(geoData[i]) );
-              } else if (geoData[i].type === 'polygon') {
+              }
+              else if (geoData[i].type === 'multipoint') {
+                ctrl.mapGeometry.push( new Multipoint(geoData[i]) );
+              }
+              else if (geoData[i].type === 'polygon') {
                 ctrl.mapGeometry.push( new Polygon(geoData[i]) );
+              }
+              else if (geoData[i].type === 'polyline') {
+                ctrl.mapGeometry.push( new Polyline(geoData[i]) );
               }
             }
           }
@@ -123,10 +245,17 @@
           if (geometry && geometry.length > 0) {
             for (var i=0; i < geometry.length; i++) {
               var symbol = null;
-              if (geometry[i] instanceof Point) {
+              if (geometry[i].type === 'point') {
                 symbol = new SimpleMarkerSymbol().setColor(new Color([0, 0, 0, 0.60]));
-              } else if (geometry[i] instanceof Polygon) {
+              }
+              else if (geometry[i].type === 'multipoint') {
+                symbol = new SimpleMarkerSymbol().setColor(new Color([0, 0, 0, 0.60]));
+              }
+              else if (geometry[i].type === 'polygon') {
                 symbol = new SimpleFillSymbol().setColor( new Color([0, 0, 0, 0.60]) );
+              }
+              else if (geometry[i].type === 'polyline') {
+                symbol = new SimpleLineSymbol().setColor( new Color([0, 0, 0, 0.60]) );
               }
 
               if (symbol) {
